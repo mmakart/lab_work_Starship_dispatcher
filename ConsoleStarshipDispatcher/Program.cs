143ef41fb942f89f75a0a85fd9725a045db172a3
Mikhail Makartsev <m.p.makartsev@gmail.com>
2021-12-11 20:16:58 +0600

    Add console app which asks for in and out files

---
+++ ConsoleStarshipDispatcher/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using StarshipDispatcher;
+
+namespace ConsoleStarshipDispatcher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter file name for input data: ");
+            var inputFilename = Console.ReadLine();
+
+            Console.Write("Enter file name for output data: ");
+            var outputFilename = Console.ReadLine();
+
+            try
+            {
+                var initialData = Parser.Parse(inputFilename);
+                var schedule = Dispatcher.CompileSchedule(initialData) as List<Departure>;
+
+                Reporter.Send(schedule, outputFilename);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}

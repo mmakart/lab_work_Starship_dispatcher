de608fe001664fef582217d6a3e6370136b47173
Mikhail Makartsev <m.p.makartsev@gmail.com>
2021-12-11 20:13:52 +0600

    Add Reporter for saving schedule into file

---
+++ StarshipDispatcher/Reporter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace StarshipDispatcher
+{
+    public static class Reporter
+    {
+        public static void Send(List<Departure> schecule, string outputFilename)
+        {
+            var serializer = new XmlSerializer(typeof(List<Departure>));
+            using (var outputFile = new FileStream(outputFilename, FileMode.OpenOrCreate))
+            {
+                serializer.Serialize(outputFile, schecule);
+            }
+        }
+    }
+}

6d50dfe127aba3208828fd4feb8435ea26aa4b87
Mikhail Makartsev <m.p.makartsev@gmail.com>
2021-12-13 18:07:51 +0600

    Reporter.cs: fix misprint in variable name

--- StarshipDispatcher/Reporter.cs
+++ StarshipDispatcher/Reporter.cs
@@ -8,12 +8,12 @@ namespace StarshipDispatcher
 {
     public static class Reporter
     {
-        public static void Send(List<Departure> schecule, string outputFilename)
+        public static void Send(List<Departure> schedule, string outputFilename)
         {
             var serializer = new XmlSerializer(typeof(List<Departure>));
             using (var outputFile = new FileStream(outputFilename, FileMode.OpenOrCreate))
             {
-                serializer.Serialize(outputFile, schecule);
+                serializer.Serialize(outputFile, schedule);
             }
         }
     }
